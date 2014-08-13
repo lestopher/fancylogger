e7e00b18862e0ec8cdac60efba1b2f0ea5d82479
Christopher Nguyen <christoph.nguyen@gmail.com>
2014-08-13 10:48:52 -0700

    Break out common logging for easier use in the future

---
+++ fancylogger.go
@@ -0,0 +1,45 @@
+package fancylogger
+
+import (
+	"io"
+	"log"
+
+	"github.com/mgutz/ansi"
+)
+
+var (
+	// Success is the color of success
+	Success = ansi.ColorCode("green")
+	// Info is the color of information
+	Info = ansi.ColorCode("white")
+	// Error is the color of a meltdown
+	Error = ansi.ColorCode("red")
+	// Warning means gun it
+	Warning = ansi.ColorCode("yellow")
+	// Reset puts the colors back the way it was
+	Reset = ansi.ColorCode("reset")
+)
+
+// FancyLogger will give you nicely printed color logs
+type FancyLogger struct {
+	Trace   *log.Logger
+	Info    *log.Logger
+	Warning *log.Logger
+	Error   *log.Logger
+}
+
+// NewFancyLogger will set up a fancy logger instance, but requires all of
+// outputs to be defined
+func NewFancyLogger(traceHandle io.Writer, infoHandle io.Writer,
+	warningHandle io.Writer, errorHandle io.Writer) *FancyLogger {
+	return &FancyLogger{
+		Trace: log.New(traceHandle, "TRACE: ",
+			log.Ldate|log.Ltime|log.Lmicroseconds|log.Lshortfile),
+		Info: log.New(infoHandle, Info+"INFO: "+Reset,
+			log.Ldate|log.Ltime|log.Lmicroseconds),
+		Warning: log.New(warningHandle, Warning+"WARNING: "+Reset,
+			log.Ldate|log.Ltime|log.Lmicroseconds|log.Lshortfile),
+		Error: log.New(errorHandle, Error+"ERROR: "+Reset,
+			log.Ldate|log.Ltime|log.Lmicroseconds|log.Lshortfile),
+	}
+}
